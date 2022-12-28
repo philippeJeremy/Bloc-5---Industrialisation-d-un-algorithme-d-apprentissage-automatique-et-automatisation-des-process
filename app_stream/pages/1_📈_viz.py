ffde8185de2443f2404947baa10f2c296158d862
jeremy <jeremy561000@gmail.com>
2022-12-28 21:19:43 +0100

    en cours viz

--- app_stream/pages/1_📈_viz.py
+++ app_stream/pages/1_📈_viz.py
@@ -33,8 +33,14 @@ mobile_ratio_state = mobile["state"].value_counts(normalize=True).round(3)
 connect = data[data["checkin_type"] == "connect"]
 connect_ratio_state = connect["state"].value_counts(normalize=True).round(3)
 
-#
+# new dataFrame incluant que les retards
+data_retard = data[data["previous_ended_rental_id"] != "NaN"]
 
+data_mobile = data_retard[data_retard["checkin_type"] == "mobile"]
+retard_mobil = data_mobile["delay_at_checkout_in_minutes"].mean()
+
+data_connect = data_retard[data_retard["checkin_type"] == "connect"]
+retard_connect = data_connect["delay_at_checkout_in_minutes"].mean()
 
 
 
@@ -74,14 +80,14 @@ fig_pie = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}
 
 fig_pie.add_trace(go.Pie( 
     values=mobile_ratio_state,
-    labels=['Non annulée', 'Annulée '],
+    labels=['Non annulée', 'Annulée'],
     marker_colors = ['#20BD2E','#FF3333'],                      
     ),
     row=1, col=1)
 
 fig_pie.add_trace(go.Pie(
     values=connect_ratio_state,
-    labels=['Non annulée', 'Annulée '],
+    labels=['Non annulée', 'Annulée'],
     marker_colors = ['#20BD2E','#FF3333'],
     ),
     row=1, col=2)
@@ -94,3 +100,72 @@ fig_pie.update_layout(
         )))
 
 st.plotly_chart(fig_pie)
+
+st.markdown("###")
+
+st.write(f'Le retard moyen pour la version mobil est de {round(retard_mobil)} minutes')
+
+st.write(f'Le retard moyen pour la version connect est de {round(retard_connect)} minutes')
+
+
+fig_connect = make_subplots(rows= 2, cols= 1,shared_xaxes = True, vertical_spacing = 0.01,)
+fig_connect.add_trace(
+    go.Histogram(
+        name='connect',
+        x = data_connect["time_delta_with_previous_rental_in_minutes"],
+        marker=dict(
+        color='blue'
+        )),
+        row = 1,
+        col = 1)
+
+fig_connect.add_trace(
+    go.Histogram(
+        name='mobil',
+        x = data_mobile["time_delta_with_previous_rental_in_minutes"],
+              marker=dict(
+        color='pink'
+        )),
+        row = 2,
+        col = 1)
+   
+
+fig_connect.update_layout( 
+                    width=1000, 
+                    height=500, 
+                    title= {'x' : 0.5},
+                    title_text="time_delta_with_previous_rental_in_minutes",
+                    )
+
+st.plotly_chart(fig_connect)
+
+fig_mobile = make_subplots(rows= 2, cols= 1,shared_xaxes = True, vertical_spacing = 0.01,)
+fig_mobile.add_trace(
+    go.Histogram(
+        name='connect',
+        x = data_connect["delay_at_checkout_in_minutes"],
+        marker=dict(
+        color='blue'
+        )),
+        row = 1,
+        col = 1)
+
+fig_mobile.add_trace(
+    go.Histogram(
+        name='mobil',
+        x = data_mobile["delay_at_checkout_in_minutes"],
+              marker=dict(
+        color='pink'
+        )),
+        row = 2,
+        col = 1)
+   
+
+fig_mobile.update_layout( 
+                    width=1000, 
+                    height=500, 
+                    title= {'x' : 0.5},
+                    title_text="delay_at_checkout_in_minutes",
+                    )
+
+st.plotly_chart(fig_mobile)
